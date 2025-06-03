89138776e952c42e6021a49a50f8ae18e925c6c4
Olha Maievska <olhamaievska91@gmail.com>
2025-06-03 12:58:12 +0200

    added Upload Dialog

--- src/components/UploadPDF.tsx
+++ src/components/UploadPDF.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -9,9 +11,45 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { UploadIcon } from "lucide-react";
+import { UploadCloud, UploadIcon } from "lucide-react";
+import { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
 
 const UploadPDF = () => {
+  const [file, setFile] = useState<File | null>(null);
+  const [url, setUrl] = useState("");
+  const [isButtonEnabled, setIsButtonEnabled] = useState(false);
+
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    const pdfFile = acceptedFiles[0];
+
+    if (!pdfFile) {
+      alert("Please upload only PDF file.");
+      return;
+    }
+
+    if (pdfFile.size > 10 * 1024 * 1024) {
+      alert("Max file size: 10 MB");
+      return;
+    }
+
+    setFile(pdfFile);
+    setUrl("");
+    setIsButtonEnabled(true);
+  }, []);
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: { "application/pdf": [".pdf"] },
+    multiple: false,
+    onDrop,
+  });
+
+  const handleUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+	setUrl(e.target.value);
+	setFile(null);
+	setIsButtonEnabled(true);
+  }
+
   return (
     <Dialog>
       <form>
@@ -22,15 +60,33 @@ const UploadPDF = () => {
           </Button>
         </DialogTrigger>
 
-        <DialogContent
-          className="sm:max-w-[425px]"
-        >
+        <DialogContent className="sm:max-w-[425px]">
           <DialogHeader>
             <DialogTitle>Upload a document</DialogTitle>
-            <DialogDescription className='hidden'> This is a description </DialogDescription>
+            <DialogDescription className="sr-only">
+              {" "}
+              This is a description{" "}
+            </DialogDescription>
           </DialogHeader>
 
           <div className="space-y-6">
+            <div
+              {...getRootProps({
+                className:
+                  "p-8 border-dashed border-2 bg-gray-50 flex flex-col items-center justify-center rounded-md cursor-pointer",
+              })}
+            >
+              <input name="file" {...getInputProps()} />
+              <UploadCloud className="w-10 h-10 text-[#FF612E]" />
+              <p className="text-slate-400 text-sm mt-2">
+                Drag and drop a PDF file here or click
+              </p>
+
+              <span className="mt-2 overflow-hidden whitespace-nowrap text-ellipsis text-sm max-w-[200px]">
+                {file?.name}
+              </span>
+            </div>
+
             <div className="flex items-center">
               <div className="flex-grow border-t border-gray-200"></div>
               <span className="flex-shrink mx-4 uppercase text-gray-600 text-xs">
@@ -49,13 +105,17 @@ const UploadPDF = () => {
               <Input
                 id="url"
                 name="url"
+                value={url}
                 className="font-light"
                 placeholder="https://cdn.openai.com/papers/gpt-4.pdf"
+                onChange={handleUrlChange}
               />
             </div>
 
             <div className="grid grid-cols-2 gap-4">
-              <Button variant="orange">Upload</Button>
+              <Button variant="orange" disabled={!isButtonEnabled}>
+                Upload
+              </Button>
               <DialogTrigger asChild>
                 <Button variant="light">Cancel</Button>
               </DialogTrigger>
